6500b427cd9daeccccc01efa9824cb5c222728c5
Andrew Oberstar <andrew.oberstar@outlook.com>
2022-01-05 18:22:58 -0600

    Adds target switch to Initialize-Adr
    This allows you to get a `.adr-dir` file created from the get-go.
    
    Fixes #2

--- ArchitectureDecisionRecords.psm1
+++ ArchitectureDecisionRecords.psm1
@@ -184,10 +184,23 @@ Initializes an ADR directory.
 
 .Description
 Initliazes an ADR directory and populates it with an initial decision to use ADRs.
+
+.Parameter Target
+If provided, creates an .adr-dir file with the target path as it's content, creates
+the target path as a directory (if it doesn't already exist) and uses this as the
+directory to populate the initial decision. If omitted, uses doc/adr as the target.
 #>
 Function Initialize-Adr {
   [CmdletBinding()]
-  Param()
+  Param(
+    [Parameter()]
+    [string] $Target
+  )
+
+  if ($Target) {
+    # If target provided, create .adir-dir file to point to it
+    Set-Content -Path '.adr-dir' -Value "$($Target)" -Encoding UTF8NoBOM
+  }
 
   $TemplateFile = Join-Path -Path $MyInvocation.MyCommand.Module.ModuleBase -ChildPath 'init.md'
   New-Adr -Title 'Record architecture decisions' -Status 'Accepted' -Template $TemplateFile
